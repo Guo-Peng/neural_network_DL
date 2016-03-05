788e2fba4e61c278387d9d41091dbac5c3d82007
郭鹏 <532957426@qq.com>
2016-03-05 12:33:26 +0800

    rename project

--- chapter_1/mnist_loader.py
+++ chapter_1/mnist_loader.py
@@ -40,7 +40,7 @@ def load_data():
     That's done in the wrapper function ``load_data_wrapper()``, see
     below.
     """
-    f = gzip.open('../../neural_newwork_DL_data/mnist.pkl.gz', 'rb')
+    f = gzip.open('../../neural_network_DL_data/mnist.pkl.gz', 'rb')
     training_data, validation_data, test_data = cPickle.load(f)
     f.close()
     return training_data, validation_data, test_data
